e03e09e601f956705309708a69c17da23b3dd92e
ythalorossy <ythalorossy@gmail.com>
2015-04-02 00:10:20 -0300

    Initial version of enemy AI

--- Assets/_scripts/Enemy.cs
+++ Assets/_scripts/Enemy.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+	public bool isDebuging = false;
+
 	[HideInInspector] 
 	public bool isFacingRight = true;
 
@@ -23,6 +25,8 @@ public class Enemy : MonoBehaviour {
 	private bool fired = false;
 
 	void Start() {
+
+		StartCoroutine(Patrol());
 	}
 
 	void FixedUpdate() {
@@ -30,8 +34,69 @@ public class Enemy : MonoBehaviour {
 		checkRecharging();
 		
 		searchPlayer();
+
+		Debug.DrawRay(
+			new Vector2(transform.position.x + ((isFacingRight) ? 1f : -1f), transform.position.y + .5f), 
+			-Vector2.up, 
+			Color.red);
 	}
 
+	IEnumerator Patrol()
+	{
+		move();
+
+		yield return new WaitForSeconds(1f);
+
+		StartCoroutine(Patrol());
+	}
+
+	/*
+	 * Check if can move.
+	 * Start one raycast in the transform.position.x + (1 or -1) looking at down.
+	 * If the hit has the tag EnemyCanMoveHere return true, otherside false;  
+	 */
+	bool canMove()
+	{
+		Vector2 rayCastPosition = new Vector2(transform.position.x + ((isFacingRight) ? 1f : -1f), transform.position.y + .5f);
+
+		RaycastHit2D hit = Physics2D.Raycast(rayCastPosition, -Vector2.up, 1f);
+
+		if (hit.collider != null) {
+
+			if (hit.collider.gameObject.tag == "EnemyCanMoveHere")
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/*
+	 * Movement
+	 */
+	void move()
+	{
+		if (!canMove())
+		{
+			Flip ();
+
+		} else {
+
+			Vector2 target = new Vector2(
+				transform.position.x + ((isFacingRight) ? 1f : -1f), 
+				transform.position.y);
+			
+			transform.position = Vector2.Lerp(
+				transform.position, 
+				target,
+				1);
+		}
+	}
+
+	/*
+	 * Check if "weapon" are recharging
+	 */
 	void checkRecharging()
 	{
 		rechargingTime += Time.deltaTime;
@@ -44,28 +109,39 @@ public class Enemy : MonoBehaviour {
 		}
 	}
 
+	/*
+	 * 
+	 */
 	void searchPlayer()
 	{
 		Vector2 rayCastPosition = new Vector2(transform.position.x, transform.position.y + .5f);
-		
+
+		/*
+		Debug.DrawLine(//transform.position,
+		               new Vector2(transform.position.x, transform.position.y + .5f),
+		               //new Vector2() (isFacingRight) ? Vector2.right : -Vector2.right, 
+		               new Vector2(
+								transform.position.x + ((isFacingRight) ? lookupTo : -lookupTo), 
+								transform.position.y + .5f),
+		               Color.green);
+		*/
+
 		// Raycast looking...
 		RaycastHit2D hit = Physics2D.Raycast(
 			rayCastPosition, 
 			(isFacingRight) ? Vector2.right : -Vector2.right, 
 			lookupTo);
-		
-		Debug.DrawRay(rayCastPosition, (isFacingRight) ? Vector2.right : -Vector2.right, Color.red);
-		
+
 		// Hit detected
 		if (hit.collider != null) {
 			
 			// Hit on Player
 			if (hit.collider.gameObject.tag == "Player")
 			{
-				// Distance from my position and hit position 
+				// Distance from my position until hit's position 
 				float distanceHit = Mathf.Abs(hit.point.y - transform.position.y);
 				
-				// Distante of hit is less or equal to distance which i see
+				// Distante of hit is less or equal to distance which i can see
 				if (distanceHit <= lookupTo)
 				{
 					// If not yet fired or recharging
@@ -78,6 +154,9 @@ public class Enemy : MonoBehaviour {
 		}
 	}
 
+	/*
+	 * 
+	 */ 
 	void shot()
 	{
 		// Set direction of power
@@ -86,8 +165,6 @@ public class Enemy : MonoBehaviour {
 		Instantiate(power, targetInstatiatePower.position, targetInstatiatePower.rotation);
 		
 		fired = true;
-		
-		Flip ();
 	}
 
 	void Flip()
