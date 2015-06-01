329d5bd53c134741af1fedc6efe17dc15a6b74ab
Ythalo Rossy <ythalorossy@gmail.com>
2015-06-01 19:58:32 -0300

    Changed gitignore

---
+++ Assets/_scripts/MoveDefault.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDefault : MonoBehaviour, IMovable {
+
+	public float distance = 3f;					// distance until the target
+	public float speed = .05f;					
+	public bool isMovementVertical = false;		// Is it vertical movement?
+	public Vector2 target;						// The target is calculated based on distance
+
+	private bool isMoving = false;
+
+	void Start () {
+
+		if (isMovementVertical) {
+			target = new Vector2(transform.position.x, transform.position.y + distance);
+		} else {
+			target = new Vector2(transform.position.x + distance, transform.position.y);
+		}
+	}
+	
+	void Update () {
+
+		if (isMoving)
+		{
+			if ( (!isMovementVertical && transform.position.x <= target.x) 
+			    || (isMovementVertical && transform.position.y <= target.y))
+			{
+				transform.position = Vector2.Lerp(
+					transform.position, 
+					target,
+					speed);
+			}
+		}
+	}
+
+	public void move ()
+	{
+		this.isMoving = true;
+	}
+}
--- Assets/_scripts/scene01/UnlockBridge.cs
+++ Assets/_scripts/scene01/UnlockBridge.cs
@@ -1,18 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UnlockBridge : MonoBehaviour, IUnlockable {
+public class UnlockBridge : MoveDefault, IUnlockable {
 
 	public void unlock () {
 
-		float distance = 3f;
-
-		Vector2 target = new Vector2(transform.position.x + distance, transform.position.y);
-
-		transform.position = Vector2.Lerp(
-			transform.position, 
-			target,
-			1);
+		this.GetComponent<MoveDefault> ().move ();
 	}
 
 }
